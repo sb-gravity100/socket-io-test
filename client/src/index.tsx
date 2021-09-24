76e55d3d33b0d19dcc43b557f167ebb367b4dcb1
Sb <sbducao0@gmail.com>
2021-09-24 19:43:08 +0000

    Done

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,44 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import { Provider } from 'react-redux';
-import { store } from './store';
-import { socket } from './socket';
-import { addMsg, changeRoom, setID, updateState } from './reducer/SocketSlice';
 
-socket.on('connect', () => {
-   store.dispatch(setID(socket.id));
-   socket.emit('SET:room', 'public', () => {
-      store.dispatch(changeRoom('public'));
-   });
-   // setInterval(() => {
-   //    socket.emit('ping', 1);
-   // }, 1000);
-});
-
-socket.on('chat', (chat) => {
-   console.log(chat);
-   store.dispatch(addMsg(chat));
-});
-
-socket.on('GET:user', (user) => {
-   let key: keyof typeof user;
-   for (key in user) {
-      store.dispatch(
-         updateState({
-            key,
-            value: user[key],
-         })
-      );
-   }
-});
+function App() {
+   return <div className="app">Hello World</div>;
+}
 
 ReactDOM.render(
    <React.StrictMode>
-      <Provider store={store}>
-         <App />
-      </Provider>
+      <App></App>
    </React.StrictMode>,
-   document.getElementById('root')
+   document.querySelector('#root')
 );
--- client/src/socket.ts
+++
@@ -1,4 +0,0 @@
-import { io, Socket } from 'socket.io-client';
-import { SocketEvents } from '../../src/events-map';
-
-export const socket: Socket<SocketEvents, SocketEvents> = io();
